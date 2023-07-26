fdc9cd6328821e3c384d440f1b6cca3fe6d388e6
codeclub20234 <134068441+codeclub20234@users.noreply.github.com>
2023-07-26 15:57:35 +0100

    added polars and plotly

--- optimise_profit.py
+++ optimise_profit.py
@@ -1,9 +1,14 @@
 import numpy as np
 import polars as pl
+import pandas as pd
+from functools import reduce
+
+df_pd = pd.read_csv('data/crypto.csv')
 
 df = pl.read_csv('data/crypto.csv', try_parse_dates=True)
 
 litecoin = df.filter(pl.col('Asset_Name') == 'Litecoin')
+litecoin_pd = df_pd[df_pd['Asset_Name'] == 'Litecoin']
 min_price_date = None
 max_price_date = None
 min_price = np.inf
@@ -13,6 +18,22 @@ min_date = None
 max_date = None
 min_price_final = np.inf
 max_price_final = 0
+
+def reducer_function(x, y):
+    if isinstance(x, float):
+        x = {'min_price': np.inf,
+             'max_price': 0,
+             'max_diff': 0}
+    x['min_price'] = min(x['min_price'], y)
+    if y > x['max_price']:
+        x['max_price'] = y
+        x['max_diff'] = max(x['max_price'] - x['min_price'], x['max_diff'])
+    return x
+
+def aggr(series):
+    return reduce(lambda x, y: reducer_function(x, y), series)
+
+print(litecoin_pd['Close'].agg(aggr))
 
 for row in litecoin.iter_rows(named = True):
     if row['Close'] < min_price:
